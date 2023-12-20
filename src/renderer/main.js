8270a680d299aa84aece357d89e6fd97c1051a91
icyn <apartmine@outlook.com>
2023-12-20 17:25:34 +0800

    fix:修改语言

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -4,7 +4,12 @@ import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import locale from 'element-ui/lib/locale/lang/en' // lang i18n
+// import locale from 'element-ui/lib/locale/lang/en' // lang i18n
+import lang from 'element-ui/lib/locale/lang/en'
+import locale from 'element-ui/lib/locale'
+
+// 设置语言
+locale.use(lang)
 
 import App from './App'
 import router from './router'
@@ -16,7 +21,7 @@ import '@/icons' // icon
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 
-Vue.use(ElementUI, { locale })
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
